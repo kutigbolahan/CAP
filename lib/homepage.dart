2f254d090ebd03a253d3e7453cc109dc5c847a7b
kuti gbolahan <kutigbolahan@ymail.com>
2020-05-26 17:59:50 +0100

    new things

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,10 +1,82 @@
 import 'package:flutter/material.dart';
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  TextEditingController titleController = TextEditingController();
+  TextEditingController detailsController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('CAP'),
+        actions: <Widget>[
+          PopupMenuButton<String>(
+            onSelected: (value){},
+            itemBuilder: (context){
+               return ['All', 'Completed', 'Incompleted'].map((option){
+                 return PopupMenuItem(
+                   value: option,
+                   child: Text(option));
+               }).toList();
+            }
+            )
+        //  Switch(value: null, onChanged: null),
+         // IconButton(icon: Icon(Icons.more_vert), onPressed: () {})
+        ],
+      ),
+      body: SafeArea(
+          child: Center(
+        child: Column(
+          children: <Widget>[
+
+          ],
+        ),
+      )),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          showDialog(
+            
+            context: context,
+            child: Dialog(
+              child: Container(
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    TextField(
+                      controller: titleController,
+                      decoration: InputDecoration(
+                        hintText: 'Title'
+                      ),
+                    ),
+                    SizedBox(height: 7,),
+                    TextField(
+                      controller: detailsController,
+                      decoration: InputDecoration(
+                        hintText: 'Details'
+                      ),
+                    ),
+                     SizedBox(height: 7,),
+                     Center(
+                       child: RaisedButton(
+                         color: Colors.black,
+                         textColor: Colors.white,
+                         onPressed: (){}, 
+                       child: Text('Add Task')
+                       ),
+                     )
+                  ],
+                ),
+              ),
+            )
+            );
+        },
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,17 @@
 import 'package:cap/homepage.dart';
+import 'package:cap/model/th.dart';
 import 'package:cap/model/todomodel.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:hive/hive.dart';
 import 'package:path_provider/path_provider.dart';
-
+const darkModeBox = 'darkModeTutorial';
 List<Box> boxList =[];
 Future<List<Box>> _openBox() async{
  var document = await getApplicationDocumentsDirectory();
   Hive.init(document.path);
   var todoBox =await Hive.openBox<TodoModel>('todoBoxName');
- var themeBox = await Hive.openBox('darkModeBox');
+ var themeBox = await Hive.openBox(darkModeBox);
   boxList.add(todoBox);
   boxList.add(themeBox);
   return boxList;
@@ -24,18 +26,28 @@ void main() async{
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+   bool isDark = false;
+   @override
+  void initState() {
+    super.initState();
+    SystemChrome.setEnabledSystemUIOverlays(SystemUiOverlay.values);
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: isDark ? Constants.darkPrimary : Constants.lightPrimary,
+      statusBarIconBrightness: isDark?Brightness.light:Brightness.dark,
+    ));
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-     
-      theme: ThemeData(
-       
-      
-       
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
+       debugShowCheckedModeBanner: false,
+      theme:  isDark ? Constants.darkTheme : Constants.lightTheme,
       home: MyHomePage(),
     );
   }
